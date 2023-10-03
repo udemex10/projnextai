329a969dd419963284c23e7eb2d428adb87dc281
udeme <udemekanem@gmail.com>
2023-10-02 19:38:09 -0500

    Updates

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: {
+    files: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"], // Paths to your files
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
