272e81e7029868b92fa98e751589ef2e0f14afe7
David P <dpfeiffer4161@gmail.com>
2023-10-29 10:50:04 -0500

    Part2-This is the first iteration. Next to adjust the visuals.

--- pages/projectspace/[projectspaceId].js
+++ pages/projectspace/[projectspaceId].js
@@ -1,33 +1,14 @@
 import React from 'react';
-import ProjectspaceCard from '../../components/projectspacecard';
-import AddProjectspaceCard from '../../components/AddProjectspaceCard';
+import WorkspaceCard from '../../components/workspacecard';
+import AddWorkspaceCard from '../../components/AddWorkspaceCard';
 import Toolbar from '../../components/Toolbar';
-import StackedLists from '../../components/StackedLists';
 
-const mockProjectspaces = [
-  {
-    _id: '1',
-    name: 'Project 1',
-    projectspaceId: '123456',
-    role: 'Classified',
-    //imageUrl: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    href: '1',
-  },
-
-  {
-    _id: '2',
-    name: 'Project 2',
-    projectspaceId: '123456',
-    role: 'UnClassified',
-    //imageUrl: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    href: '2',
-  },
-
+const mockWorkspaces = [
+  { _id: '1', name: 'Workspace 1', description: 'This is workspace 1' },
+  { _id: '2', name: 'Workspace 2', description: 'This is workspace 2', organizationId: 'org1' },
 ];
 
-
-const ProjectspaceDashboard = ({ handleDelete }) => {
-  
+const Dashboard = ({ handleDelete }) => {
   return (
     <div className="flex flex-col pb-8 bg-gray-900 text-white h-screen w-full">
       <Toolbar
@@ -40,14 +21,13 @@ const ProjectspaceDashboard = ({ handleDelete }) => {
       />
 
       <div className="pl-8 pr-8 flex flex-wrap space-x-8 mt-6">
-        {mockProjectspaces.map(projectspace => (
-          <StackedLists key={projectspace._id} projectspace={projectspace} onDelete={handleDelete} />
+        {mockWorkspaces.map(workspace => (
+          <WorkspaceCard key={workspace._id} workspace={workspace} onDelete={handleDelete} />
         ))}
-        <AddProjectspaceCard />
+        <AddWorkspaceCard />
       </div>
     </div>
   );
-  
 };
 
-export default ProjectspaceDashboard;
+export default Dashboard;
