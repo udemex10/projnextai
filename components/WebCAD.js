d4d5abaccb83c590e518ba9cbfb85f607aab3e3e
Udeme Ekanem <udemekanem@gmail.com>
2023-11-08 23:12:11 -0600

    Cad integration

--- components/WebCAD.js
+++ components/WebCAD.js
@@ -1,7 +1,7 @@
 function WebCAD() {
   return (
     <iframe
-      src="http://localhost:3001/combined.html"
+      src="http://localhost:3001/index.html"
       width="100%"
       height="100%"
       frameBorder="0"
